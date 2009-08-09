6ed6200d7ed5f25e4eb0d3688cd6f8eeb71796f9
Joerg Kiegeland <joerg@kiegeland.com>
2009-08-09 12:36:02 +0000

    initial commit
    git-svn-id: svn://localhost/jjkpp@4 1fe8b304-53d4-c445-bb71-d45f26d273d2

---
+++ jjkpp.jdt.annotations/src/jjkpp/jdt/annotations/CanBeNullParam22.java
@@ -0,0 +1,16 @@
+package jjkpp.jdt.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates that the annotated method is a test method. This annotation should
+ * be used only on parameterless static methods.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target( { ElementType.METHOD, ElementType.CONSTRUCTOR })
+public @interface CanBeNullParam22 {
+
+}
