2c218ebac8b06040d333debe70bd6e7e8349d77a
Joerg Kiegeland <joerg@kiegeland.com>
2009-11-22 21:43:07 +0000

    NonNull parameter may be assigned null within method
    git-svn-id: svn://localhost/jjkpp@61 1fe8b304-53d4-c445-bb71-d45f26d273d2

--- jjkpp.javaproject.test/src/topPackage/assignment/ParameterAssignmentTest.java
+++ jjkpp.javaproject.test/src/topPackage/assignment/ParameterAssignmentTest.java
@@ -5,14 +5,26 @@ import jjkpp.jdt.annotations.NonNullParam1;
 
 public class ParameterAssignmentTest {
 
+	private void testDefaultParameterVariableAssignNull(String s1) {
+		s1.toString(); /* OK */;
+		s1 = null/* OK */;  
+		s1.toString(); /* error1 */;
+	}
+	
 	@NonNullParam1
 	private void testNonNullParameterVariableAssignNull(String s1) {
-		s1 = null/* error0 */;
+		s1.toString(); /* OK */;      
+		s1 = null/* OK */;
+		s1.toString(); /* error0 */;
 	}
 
 	@CanBeNullParam1
 	private void testCanBeNullParameterVariableAssignNull(String s1) {
+		s1.toString(); /* error0 */; 
+		s1="";
+		s1.toString(); /* OK */; 
 		s1 = null/* OK */;
+		s1.toString(); /* error0 */;
 	}
 
 }
