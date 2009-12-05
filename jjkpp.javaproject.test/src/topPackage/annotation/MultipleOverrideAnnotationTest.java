7e2cb15a21947995be28524f18cbb217a233fda3
Joerg Kiegeland <joerg@kiegeland.com>
2009-12-05 00:54:55 +0000

    fixed annotations bug at multiple inheritance
    git-svn-id: svn://localhost/jjkpp@82 1fe8b304-53d4-c445-bb71-d45f26d273d2

---
+++ jjkpp.javaproject.test/src/topPackage/annotation/MultipleOverrideAnnotationTest.java
@@ -0,0 +1,40 @@
+package topPackage.annotation;
+
+import jjkpp.jdt.annotations.CanBeNull;
+import jjkpp.jdt.annotations.NonNull;
+
+public class MultipleOverrideAnnotationTest {
+
+	@CanBeNull
+	protected String testReturn() {
+		return "";
+	}
+}
+
+abstract class MultipleOverrideAnnotationTest2 extends MultipleOverrideAnnotationTest {
+
+	@NonNull
+	abstract protected String testReturn();
+
+}
+
+class MultipleOverrideAnnotationTest3 extends MultipleOverrideAnnotationTest2 {
+
+	@Override
+	protected String testReturn() {
+		return null; /* error1 easy */
+	}
+
+}
+
+class MultipleOverrideAnnotationTest4 extends MultipleOverrideAnnotationTest2 {
+
+	@CanBeNull
+	/* error0 */
+	@Override
+	protected String testReturn() {
+		return "";
+
+	}
+
+}
