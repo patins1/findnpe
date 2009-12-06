6a049ba3ee315d230e4484563366250c6f727a09
Joerg Kiegeland <joerg@kiegeland.com>
2009-12-06 10:23:46 +0000

    NonNullByDefault applies also to nested types
    git-svn-id: svn://localhost/jjkpp@92 1fe8b304-53d4-c445-bb71-d45f26d273d2

--- jjkpp.javaproject.test/src/topPackage/annotation/ClassAnnotationTest.java
+++ jjkpp.javaproject.test/src/topPackage/annotation/ClassAnnotationTest.java
@@ -1,18 +1,44 @@
 package topPackage.annotation;
 
-import jjkpp.jdt.annotations.CanBeNull;
-import jjkpp.jdt.annotations.NonNull;
 import jjkpp.jdt.annotations.NonNullByDefault;
 
 @NonNullByDefault
 public class ClassAnnotationTest {
 
-	String testReturnNull() {
+	protected String testReturnNull() {
 		return null; /* error1 easy */
 	}
 
-	void testReturnNull(String param1) {
+	protected void testReturnNull(String param1) {
 		testReturnNull(null); /* error1 easy */
+	}
+	
+
+	class ClassAnnotationTest1 {
+
+		protected String testReturnNull() {
+			return null; /* error1 easy */
+		}
+
+		protected void testReturnNull(String param1) {
+			testReturnNull(null); /* error1 easy */
+		}
+
+	}
+	
+	void test() {
+		new  Object() {
+
+			protected String testReturnNull() {
+				return null; /* error1 easy */
+			}
+
+			protected void testReturnNull(String param1) {
+				testReturnNull(null); /* error1 easy */
+			}
+
+		};
+		
 	}
 
 }
