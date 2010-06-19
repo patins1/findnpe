3d2e1fb9345d4eaea6875347adf871adb69194e3
Joerg Kiegeland <joerg@kiegeland.com>
2010-06-19 00:50:22 +0000

    refactoring: renaming to pingpong
    git-svn-id: svn://localhost/jjkpp@156 1fe8b304-53d4-c445-bb71-d45f26d273d2

--- jjkpp.jdt.test/src/pingpong/jdt/test/Activator.java
+++ jjkpp.jdt.test/src/pingpong/jdt/test/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "jjkpp.jdt.test";
+	public static final String PLUGIN_ID = "pingpong.jdt.test";
 
 	// The shared instance
 	private static Activator plugin;
