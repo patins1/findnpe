0984df6f582991cb79243bda45d75ae13460eb9d
Joerg Kiegeland <joerg@kiegeland.com>
2010-12-28 13:29:25 +0000

    initial commit of second java test project
    git-svn-id: svn://localhost/jjkpp@226 1fe8b304-53d4-c445-bb71-d45f26d273d2

---
+++ pingpong.javaproject.test2/src/binaryTopPackage/BinaryData.java
@@ -0,0 +1,14 @@
+package binaryTopPackage;
+
+import findnpe.annotations.CanBeNull;
+import findnpe.annotations.NonNull;
+
+public class BinaryData {
+
+	@NonNull
+	public void doS2(@CanBeNull String param1) {
+	}
+
+	public void doS3(@NonNull String param1) {
+	}
+}
